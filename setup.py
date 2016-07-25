f4e16ba3cf9ad5b4211417f1038c2c013d5e3c4e
general electrix <general.electrix@gmail.com>
2016-07-25 16:08:31 -0700

    ENH: initiali commit, completed render server

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from Cython.Build import cythonize
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+import numpy
+
+version = '1.0.0'
+
+requires = []
+
+setup(
+    name='game_loop',
+    packages=['game_loop'],
+    install_requires=requires,
+    license='MIT',
+    version=version,
+)
